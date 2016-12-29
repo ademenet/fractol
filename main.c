09e7b8d6f0537498b468504d99348e42776b57a6
moannuo <ademenet@student.42.fr>
2016-12-29 16:49:02 +0100

    fractol: set main function to work with put_image

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 **.Error handling and displaying
 */
 
-void	ft_error(char *s)
-{
-	if (s == NULL)
-		perror("error");
-	else
-	{
-		ft_putstr("error: ");
-		ft_putstr(s);
-		ft_putstr(".\n");
-	}
-	exit(-1);
-}
+// void	ft_error(char *s)
+// {
+// 	if (s == NULL)
+// 		perror("error");
+// 	else
+// 	{
+// 		ft_putstr("error: ");
+// 		ft_putstr(s);
+// 		ft_putstr(".\n");
+// 	}
+// 	exit(-1);
+// }
 
 int		main(void)
 {
@@ -23,10 +23,12 @@ int		main(void)
 
 	// if (ac == 2)
 	// {
-		env.mlx = mlx_init();
-		env.win = mlx_new_window(env.mlx, WIN_X, WIN_Y, "Fractol");
-		env.im = mlx_new_image(env.mlx, WIN_X, WIN_Y);
-		env.im_buf = mlx_get_data_addr(env.im, &env.bpp, &env.sl, &env.endian);
+	env.mlx = mlx_init(); // initialize mlx server
+	env.win = mlx_new_window(env.mlx, WIN_WIDTH, WIN_HEIGHT, "Fractol"); // set a window
+    // Instead of followings functions, we'll use put_image now:
+	// env.im = mlx_new_image(env.mlx, WIN_WIDTH, WIN_HEIGHT); // create a new image in memory
+	// env.im_buf = mlx_get_data_addr(env.im, &env.bpp, &env.sl, &env.endian);
+	put_image(&env); // create, process and display image
 	// }
 	// else
 	// 	ft_error("too much arguments or not enough");
