a27a9aab3764e5b7be67e9c1c3aa2beed403741f
moannuo <alain.demenet@gmail.com>
2017-03-06 19:27:14 +0100

    fractol: start parsing WIP

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include "fractol.h"
 
 /*
-**.Error handling and displaying
+** Error handling and displaying
 */
+
+void		display_usage(void)
+{
+	ft_printf("| Please use Fract'ol like this:\n");
+	ft_printf("| \t./fractol <fractal> [options]\n");
+	ft_printf("| Fractals: mandelbrot, julia, third\n");
+}
+
+char		*parser(char **argv, char *arg_parse)
+{
+	if (ft_strcmp("mandelbrot", argv[1]) == 0)
+		arg_parse[0] = 1;
+	else if (ft_strcmp("julia", argv[1]) == 0)
+		arg_parse[1] = 1;
+	else if (ft_strcmp("third", argv[1]) == 0)
+		arg_parse[2] = 1;
+	else
+		display_usage();
+	return arg_parse;
+}
 
 // void	ft_error(char *s)
 // {
@@ -17,15 +37,22 @@
 // 	exit(-1);
 // }
 
-int		main(void)
+
+
+int		main(int argc, char **argv)
 {
 	t_env	env;
 
 	// TODO: parser!!!
+	if (argc < 2)
+		display_usage();
+	else
+		parser(argv, env.arg);
 	// TODO: error handling
 	// BONUS: cuda
 	// if (ac == 2)
 	// {
+	ft_printf("%d", env.arg[0]);
 	env.mlx = mlx_init(); // initialize mlx server
 	env.win = mlx_new_window(env.mlx, WIN_WIDTH, WIN_HEIGHT, "Fract'ol"); // set a window
     // Instead of followings functions, we'll use put_image now:
