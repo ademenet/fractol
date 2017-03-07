976a6eb8641704aada5c9cda8c475a543599c761
moannuo <alain.demenet@gmail.com>
2017-03-07 18:11:23 +0100

    update: rename structure and initializing values depending on fractal

--- fractals.c
+++ fractals.c
@@ -1,22 +1,57 @@
 #include "fractol.h"
 
+/*
+** `fal` stands for fractal, it refers to everything specific to one fractal:
+** Mandelbrot, Julia, etc.
+*/
+
+/*
+** Set the limit for better display.
+*/
+
+void		set_limits(t_env *env, t_fal *fal)
+{
+	if (env->arg[0] == 1)
+	{
+		fal->x1 = -2.1;
+		fal->x2 = 0.6;
+		fal->y1 = -1.2;
+		fal->y2 = 1.2;
+	}
+	else if (env->arg[1] == 1)
+	{
+		fal->x1 = -1;
+		fal->x2 = 1;
+		fal->y1 = -1.2;
+		fal->y2 = 1.2;
+	}
+	else if (env->arg[2] == 1)
+	{
+		// TODO: limits for last fractal
+		fal->x1 = -1;
+		fal->x2 = 1;
+		fal->y1 = -1.2;
+		fal->y2 = 1.2;
+	}
+}
+
+/*
+** Skim the complete plan in order to compute if each point of coordinates
+** (x, y) belongs to the given fractal set.
+*/
+
 void        fractals_compute(t_env *env)
 {
 	int		x;
 	int		y;
-	t_mdl	mdl;
+	t_fal	fal;
 
 	x = -1;
-	// TODO: those (x1, x2, y1, y2) depends on fractal to display, need to put
-	// them in variables
-	mdl.x1 = -2.1;
-	mdl.x2 = 0.6;
-	mdl.y1 = -1.2;
-	mdl.y2 = 1.2;
-	mdl.settings.zoom_x = WIN_WIDTH / (mdl.x2 - mdl.x1);
-	mdl.settings.zoom_y = WIN_HEIGHT / (mdl.y2 - mdl.y1);
+	set_limits(env, &fal);
+	fal.settings.zoom_x = WIN_WIDTH / (fal.x2 - fal.x1);
+	fal.settings.zoom_y = WIN_HEIGHT / (fal.y2 - fal.y1);
 	// TODO: set max_iter as a variable
-	mdl.settings.max_iter = 50; // Also called "rang" in french
+	fal.settings.max_iter = 100; // Also called "rang" in french
 	// In the following loops we are going to compute for each point if it
 	// belongs to the corresponding fractal set.
 	while(++x < WIN_WIDTH)
@@ -25,7 +60,9 @@ void        fractals_compute(t_env *env)
 		while (++y < WIN_HEIGHT)
 		{
 			// TODO: call the right function according to the user preferences
-			mandelbrot(x, y, &mdl, env);
+			// mandelbrot(x, y, &fal, env);
+			// julia(x, y, &fal, env);
+			burningship(x, y, &fal, env);
 		}
 	}
 }
--- mandelbrot.c
+++ mandelbrot.c
@@ -8,32 +8,27 @@
 ** it belongs to Mandelbrot's set.
 */
 
-void        mandelbrot(int x, int y, t_mdl *mdl, t_env *env)
+void        mandelbrot(int x, int y, t_fal *fal, t_env *env)
 {
 	int		i;
 	float	tmp;
 
 	i = -1;
-	mdl->c_r = x / mdl->settings.zoom_x + mdl->x1;
-	mdl->c_i = y / mdl->settings.zoom_y + mdl->y1;
-	mdl->z_r = 0;
-	mdl->z_i = 0;
-	// TODO: find why it's 4...
-	while ((mdl->z_r * mdl->z_r + mdl->z_i * mdl->z_i) < 4
-			&& ++i < mdl->settings.max_iter) // while(module < 4 and i < max iterations)
+	fal->c_r = x / fal->settings.zoom_x + fal->x1;
+	fal->c_i = y / fal->settings.zoom_y + fal->y1;
+	fal->z_r = 0;
+	fal->z_i = 0;
+	while ((fal->z_r * fal->z_r + fal->z_i * fal->z_i) < 4
+			&& ++i < fal->settings.max_iter) // while(module < 4 and i < max iterations)
 	{
-		tmp = mdl->z_r;
-		mdl->z_r = mdl->z_r * mdl->z_r - mdl->z_i * mdl->z_i + mdl->c_r;
-		mdl->z_i = 2 * mdl->z_i * tmp + mdl->c_i;
+		tmp = fal->z_r;
+		fal->z_r = fal->z_r * fal->z_r - fal->z_i * fal->z_i + fal->c_r;
+		fal->z_i = 2 * fal->z_i * tmp + fal->c_i;
 	}
 	// We test if we reach max_iter or not. If it is the case then the point
 	// does not belong to Mandelbrot's set and it's drew in a certain color.
-	if (i == mdl->settings.max_iter)
-	{
+	if (i == fal->settings.max_iter)
 		put_pixel(env, x, y, 000000000);
-	}
 	else
-	{
-		put_pixel(env, x, y, i * 255 / mdl->settings.max_iter);
-	}
+		put_pixel(env, x, y, i * 255 / fal->settings.max_iter);
 }
